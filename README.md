245723b96eeb32095c4ca0e0af4e6ad68cb1b675
stevejkang <iam@juneyoung.io>
2025-02-11 22:11:12 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# .bbodek
+A dotfile directory for storing sensitive keys and credentials with an all-in-one bash script to initialize BBODEK local backend development environment using infisical.
+
+## Prerequisites
+- [Infisical](https://infisical.com/) for retrieving keys and credentials
+- [Tailscale](https://tailscale.com/) for SSH connection
+
+## Installation
+```bash
+# Clone
+cd ~
+git clone https://github.com/thebbodek/.bbodek.git
+
+# Infisical CLI
+brew install infisical/get-cli/infisical
+infisical login
+
+# Installation
+chmod +x ~/.bbodek/initialize.sh
+~/.bbodek/initialize.sh
+```
+
+## Guide
+### Update AWS Credentials
+1. Update environment variable in infisical `/org` directory.
+2. Define environment variable in `~/.bbodek/aws/credentials.[env].template`.
+3. Run `~/.bbodek/initialize.sh` to sync changes.
+
+### Update SSH Config
+1. Update `~/.bbodek/ssh/config`.
+
+### Add Private Key
+1. Update environment variable in infisical `/_keys` directory.
+2. Define environment variable in `~/.bbodek/keys/[name].pem.template`.
+3. Run `~/.bbodek/initialize.sh` to sync changes.
---
+++ initialize.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# ssh
+if [ -f ~/.ssh/config ]; then
+    if ! grep -q "^Include ~/.bbodek/ssh/config" ~/.ssh/config; then
+        sed -i '' '1i\
+# Added by .bbodek/initialize.sh\
+Include ~/.bbodek/ssh/config\
+\
+' ~/.ssh/config
+fi
+else
+    echo "Include ~/.bbodek/ssh/config" > ~/.ssh/config
+    chmod 600 ~/.ssh/config
+fi
+
+# aws
+cat > ~/.bbodek/aws/credentials.temp << EOF
+### BBODEK ###
+
+$(infisical run --env=local --path=/org -- envsubst < ~/.bbodek/aws/credentials.local.template)
+
+$(infisical run --env=dev --path=/org -- envsubst < ~/.bbodek/aws/credentials.dev.template)
+
+$(infisical run --env=prod --path=/org -- envsubst < ~/.bbodek/aws/credentials.prod.template)
+
+### BBODEK ###
+EOF
+if grep -q "### BBODEK ###" ~/.aws/credentials; then
+    echo "Overwriting ~/.bbodek/aws/credentials.*.template -> ~/.aws/credentials"
+    START_LINE=$(grep -n "### BBODEK ###" ~/.aws/credentials | head -1 | cut -d: -f1)
+    END_LINE=$(grep -n "### BBODEK ###" ~/.aws/credentials | tail -1 | cut -d: -f1)
+
+    head -n $((START_LINE-1)) ~/.aws/credentials > ~/.aws/credentials.tmp
+    cat ~/.bbodek/aws/credentials.temp >> ~/.aws/credentials.tmp
+    tail -n +$((END_LINE+1)) ~/.aws/credentials >> ~/.aws/credentials.tmp
+    mv ~/.aws/credentials.tmp ~/.aws/credentials
+else
+    echo "Appending ~/.bbodek/aws/credentials.*.template -> ~/.aws/credentials"
+    cat ~/.bbodek/aws/credentials.temp >> ~/.aws/credentials
+fi
+rm ~/.bbodek/aws/credentials.temp
+
+# keys
+for template in ~/.bbodek/keys/*.pem.template; do
+    if [ -f "$template" ]; then
+        output_file="${template%.template}"
+        echo "Processing $template -> $output_file"
+        chmod 600 "$output_file"
+        infisical run --env=local --path=/_keys -- envsubst < "$template" > "$output_file"
+        chmod 400 "$output_file"
+    fi
+done
